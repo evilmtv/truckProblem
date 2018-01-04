fc5c38daf0ebce0f4be82ff9662f6d541d6c7808
Jun Hao <e0012616@u.nus.edu>
2018-01-05 01:29:57 +0800

    Comments and python naming convention

--- Attempt1.py
+++ Attempt1.py
@@ -10,32 +10,35 @@ from datetime import datetime
 import time
 import random
 
-def zerolistmaker(n):
+def zero_list_maker(n):
     listofzeros = [0] * n
     return listofzeros
 
 # Print current time on computer
 print (str(datetime.now()), '\n')
 # Implement simple timer
-current_milli_time = lambda: int(round(time.time() * 1000)) 
+current_milli_time = lambda: int(round(time.time() * 1000))
 startTime = current_milli_time()
 
 # Variable Declarations
-currentPosition = 0
-fuelOnHand = 0
-fuelTaken = 0
-fuelUsed = 0
-tryCount = 0 # Furthest checked location
+current_position = 0
+fuel_on_hand = 0
+fuel_taken = 0
+fuel_used = 0
+try_count = 0 # Furthest checked location
 n = random.randint(1, 10)
 print ('Distance to destination =', n, '\n')
 steps = 0
 state = 'exploring'
-fuelMap = zerolistmaker(2) # Taking fuelMap[0] as start point, fuelMap[1] as position 1 etc..
+fuel_map = zero_list_maker(2) # Taking fuel_map[0] as start point, fuel_map[1] as position 1 etc..
 
 
-def geometricSum():
-    geoN = tryCount - currentPosition
-    if ((tryCount - currentPosition) == 0):
+# To explore 3 units away from start point, the truck has to first bring 7 units of fuel to 1 unit away from start point
+# To explore 4 units away from start point, the truck has to first bring 19 units of fuel to 1 unit away from start point
+# The series follows a geometric sum where the nth term can easily be called using this function and n is defined by try_count and current_position
+def geometric_sum():
+    geoN = try_count - current_position
+    if ((try_count - current_position) == 0):
         return 3
     else:
         ratio = 3
@@ -43,140 +46,140 @@ def geometricSum():
         progression = [start * ratio**i for i in range(geoN)]
         return(3 + sum(progression))
 
-def takeFuel(qty):
-    global fuelOnHand
-    global fuelMap
+def take_fuel(qty):
+    global fuel_on_hand
+    global fuel_map
     global steps
-    global fuelTaken
-    if (currentPosition > 0):
-        if (fuelMap[currentPosition] >= qty):
-            fuelMap[currentPosition] -= qty
-            fuelOnHand += qty
+    global fuel_taken
+    if (current_position > 0):
+        if (fuel_map[current_position] >= qty):
+            fuel_map[current_position] -= qty
+            fuel_on_hand += qty
             steps += 1
-    elif (currentPosition == 0):
-        fuelOnHand += qty
-        fuelTaken += qty
+    elif (current_position == 0):
+        fuel_on_hand += qty
+        fuel_taken += qty
     else:
-        print ('\nERROR at takeFuel\n')
-    print (fuelMap)
-    print('Fuel:', fuelOnHand, '| Position:', currentPosition, '| Task: Refueling', '| Steps:', steps, '| State:', state)
-          
-def moveAndDrop(direction):
-    global fuelOnHand
-    global fuelTaken
-    global fuelUsed
+        print ('\nERROR at take_fuel\n')
+    print (fuel_map)
+    print('Fuel:', fuel_on_hand, '| Position:', current_position, '| Task: Refueling', '| Steps:', steps, '| State:', state)
+
+def move_and_drop(direction):
+    global fuel_on_hand
+    global fuel_taken
+    global fuel_used
     global steps
-    global currentPosition
-    
+    global current_position
+
     if (direction == 'back'):
-        currentPosition -= 1
+        current_position -= 1
     elif (direction == 'forward'):
-        currentPosition += 1
+        current_position += 1
 
-    fuelOnHand -= 1
-    fuelUsed += 1
-    if (currentPosition == 0):
-        fuelTaken -= fuelOnHand
+    fuel_on_hand -= 1
+    fuel_used += 1
+    if (current_position == 0):
+        fuel_taken -= fuel_on_hand
     else:
-        fuelMap[currentPosition] += fuelOnHand
-    fuelOnHand = 0
+        fuel_map[current_position] += fuel_on_hand
+    fuel_on_hand = 0
     steps += 1
-    print (fuelMap)
-    print('Fuel:', fuelOnHand, '| Position:', currentPosition, '| Task: Moving', direction, '| Steps:', steps, '| State:', state)
+    print (fuel_map)
+    print('Fuel:', fuel_on_hand, '| Position:', current_position, '| Task: Moving', direction, '| Steps:', steps, '| State:', state)
 
-def carryFromPrevious():
-    global currentPosition
-    global fuelOnHand
-    global fuelTaken
-    global fuelUsed
+def carry_from_previous():
+    global current_position
+    global fuel_on_hand
+    global fuel_taken
+    global fuel_used
     global steps
-    global fuelMap
-    
-    tempGeometricSum = geometricSum()
-    
-    while (fuelMap[currentPosition] < tempGeometricSum):
+    global fuel_map
+
+    temp_geometric_sum = geometric_sum()
+
+    while (fuel_map[current_position] < temp_geometric_sum):
         # Fill up truck
-        takeFuel(1)
+        take_fuel(1)
         # Move truck backwards and drop fuel
-        moveAndDrop('back')
+        move_and_drop('back')
         # Fill up truck
-        takeFuel(3)
+        take_fuel(3)
         # Move truck forward and drop fuel
-        moveAndDrop('forward')
-    
-    if (currentPosition == tryCount):
+        move_and_drop('forward')
+
+    if (current_position == try_count):
         return()
-        
+
     else:
         # Fill up truck
-        takeFuel(3)
+        take_fuel(3)
         # Move truck forward and drop fuel
-        moveAndDrop('forward')
-        
-        carryFromPrevious()
-        return()
-        
+        move_and_drop('forward')
 
-# Main Code
+        carry_from_previous()
+        return()
+
+
 print ('BEGIN \n')
-      
-# Attempt 'if n = 1'
+
+# Checking if n == 1
 # Fill up truck
-takeFuel(2)
+take_fuel(2)
 # Move truck forward and drop fuel
-moveAndDrop('forward')
+move_and_drop('forward')
 # Check if destination reached
-if (currentPosition == n):
+if (current_position == n):
     state = 'end'
 else:
     state = 'returning'
     # Update map
-    fuelMap.extend(zerolistmaker(1))
+    fuel_map.extend(zero_list_maker(1))
     # Update latest checked distance
-    tryCount = 1
+    try_count = 1
 
+# Main Loop
 while (state != 'end'):
-    
+
     while (state == 'returning'):
         # Fill up truck
-        takeFuel(fuelMap[currentPosition])
+        take_fuel(fuel_map[current_position])
         # Move truck
-        moveAndDrop('back')
-        
-        if (currentPosition == 0):
-            state = 'exploring'
-        
-    while (state == 'exploring'):
-        
-        if (currentPosition == 0):
-            # Fill up truck
-            takeFuel(3)
-             #Move truck forward and drop fuel
-            moveAndDrop('forward')
-            carryFromPrevious()
+        move_and_drop('back')
 
-        if (currentPosition == (tryCount + 1)):
-            tryCount += 1
-            
-            if (currentPosition == n):
+        if (current_position == 0):
+            state = 'exploring'
+
+    while (state == 'exploring'):
+
+        if (current_position == 0):
+            # Fill up truck
+            take_fuel(3)
+             #Move truck forward and drop fuel
+            move_and_drop('forward')
+            carry_from_previous()
+
+        if (current_position == (try_count + 1)):
+            try_count += 1
+
+            if (current_position == n):
                 state = 'end'
-                
+
             else:
                 # Update map
-                fuelMap.extend(zerolistmaker(1))
+                fuel_map.extend(zero_list_maker(1))
                 print ('\nDestination not reached, insufficient fuel to progress, returning to base\n')
                 state = 'returning'
-                
-        elif (currentPosition == tryCount):
+
+        elif (current_position == try_count):
             # Fill up truck
-            takeFuel(2)
+            take_fuel(2)
             # Move truck forward and drop fuel
-            moveAndDrop('forward')
+            move_and_drop('forward')
 
 print ('\nCurrent Position is at destination, Mission Complete!\n')
 
 print ('Mission took', (current_milli_time() - startTime), 'ms\n')
 
 print ('::Results::')
-print ('Fuel taken from start:', fuelTaken)
-print ('Fuel actually used:', fuelUsed)
+print ('Fuel taken from start:', fuel_taken)
+print ('Fuel actually used:', fuel_used)
